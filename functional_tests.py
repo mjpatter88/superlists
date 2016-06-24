f5dacbec47eaa3f205aff6117a2973537da587e8
Michael Patterson <mjpatter88@gmail.com>
2016-06-23 22:05:07 -0500

    Refactor to match pep8

--- functional_tests.py
+++ functional_tests.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
 
+class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.PhantomJS()
         self.browser.implicitly_wait(3)
@@ -27,11 +27,10 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
-                "New to-do item did not appear in table.")
-
+                        "New to-do item did not appear in table.")
 
         self.fail('Finish the test!')
 
+
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-        
