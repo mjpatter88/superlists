a42e65c5f22e2684bc2665ad7a05be97a64027e2
Michael Patterson <mjpatter88@gmail.com>
2016-06-25 17:02:52 -0500

    Extend functional test to test that multiple items can be added to the list.

--- lists/tests.py
+++ lists/tests.py
@@ -1,18 +1,32 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
-from django.http import HttpResponse
+from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
 
-class HomePageTest(TestCase):
 
+class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpResponse()
+        request = HttpRequest()
+        request.method = 'GET'
         response = home_page(request)
-        expected_html = render_to_string('home.html')
+        expected_html = render_to_string('home.html', request=request)
+        self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        item_text = 'A new list item'
+        request.POST['item_text'] = item_text
+
+        response = home_page(request)
+
+        self.assertIn(item_text, response.content.decode())
+
+        expected_html = render_to_string('home.html', {'new_item_text': item_text}, request=request)
         self.assertEqual(response.content.decode(), expected_html)
