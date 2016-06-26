b9b42f056e6c2233b6c4921f44a52260bc1facf3
Michael Patterson <mjpatter88@gmail.com>
2016-06-26 00:04:14 -0500

    Extend FT to cover unique lists for unique visitors.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -27,13 +27,35 @@ class NewVisitorTest(LiveServerTestCase):
 
         input_box.send_keys('Buy peacock feathers')
         input_box.send_keys(Keys.ENTER)
+        list_url = self.browser.current_url
+        self.assertRegex(list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys('Use peacock feathers to make a fly')
         input_box.send_keys(Keys.ENTER)
-
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # New user comes along
+        self.browser.quit()
+        self.browser = webdriver.PhantomJS()
+        self.browser.get(self.live_server_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Buy milk')
+        input_box.send_keys(Keys.ENTER)
+
+        new_list_url = self.browser.current_url
+        self.assertRegex(new_list_url, '/lists/.+')
+        self.assertNotEqual(new_list_url, list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
 
         self.fail('Finish the test!')
 
