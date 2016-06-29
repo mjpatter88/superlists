9ab432c514653b6020e1fcc5e90b757516f652d6
Michael Patterson <mjpatter88@gmail.com>
2016-06-28 19:29:41 -0500

    refactor views and urls to have a separate endpoint for creating a new list.

--- lists/tests.py
+++ lists/tests.py
@@ -19,34 +19,6 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html', request=request)
         self.assertEqual(response.content.decode(), expected_html)
 
-    def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        item_text = 'A new list item'
-        request.POST['item_text'] = item_text
-
-        home_page(request)
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, item_text)
-
-    def test_home_page_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        item_text = 'A new list item'
-        request.POST['item_text'] = item_text
-
-        response = home_page(request)
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
-
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
@@ -80,3 +52,19 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+class NewListTest(TestCase):
+    def test_saving_a_POST_request(self):
+        item_text = 'A new list item'
+        self.client.post('/lists/new', data={'item_text': item_text})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, item_text)
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response,'/lists/the-only-list-in-the-world/')
