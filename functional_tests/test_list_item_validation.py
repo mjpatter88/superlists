d0d12eb5bfe9d835b25309207e969dffc1c4f593
Michael Patterson <mjpatter88@gmail.com>
2016-08-03 22:36:21 -0500

    Adjust new list view to do model validation.

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -6,7 +6,7 @@ class ItemValidationTest(FunctionalTest):
         self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You can't have an emplty list item")
+        self.assertEqual(error.text, "You can't have an empty list item.")
 
         self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
         self.check_for_row_in_list_table('1: Buy milk')
@@ -15,7 +15,7 @@ class ItemValidationTest(FunctionalTest):
 
         self.check_for_row_in_list_table('1: Buy milk')
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You can't have an emplty list item")
+        self.assertEqual(error.text, "You can't have an empty list item.")
 
         self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
         self.check_for_row_in_list_table('1: Buy milk')
