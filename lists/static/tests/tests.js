b12f129bfa9af00372d819abba7eb46118fde131
Michael Patterson <mjpatter88@gmail.com>
2016-12-19 22:43:22 -0600

    Add simple js and tests to verify that error message is removed once typing starts.

--- lists/static/tests/tests.js
+++ lists/static/tests/tests.js
@@ -1,3 +1,10 @@
-QUnit.test("smoke test", function(assert) {
-    assert.equal(1, 1, "Math Works!");
+QUnit.test("errors should be hidden on keypress", function(assert) {
+    $('input').trigger('keypress');
+    assert.equal($('.has-error').is(':visible'), false);
+})
+// The above test fails due to changes in the QUnit lib. Since the input gets reset between each test,
+// the event listener that we add in list.js gets dropped and fails to respond to the event.
+
+QUnit.test("errors should not be hidden on keypress", function(assert) {
+    assert.equal($('.has-error').is(':visible'), true);
 })
